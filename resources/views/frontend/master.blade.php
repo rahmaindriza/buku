7f21e42c28890903546a887a33c67b653b17b80c
rahmaindriza <indriza37@gmail.com>
2025-07-03 00:31:37 +0700

    Membuat migration Role (admin & user)

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -9,16 +9,16 @@
 </head>
 <body class="bg-gray-100">
 
-    {{-- navbar --}}
-    <nav class="bg-blue-600 text-white py-4">
-        <div class="container max-w-6x1 mx-auto flex justify-between items-center">
-            <a href="{{ route('homepage')}}" class="text-2x1 font-bold">RI-Library</a>
-            <div>
-                <a href="{{ route('homepage') }}" class="px-4">Homepage</a>
-                <a href="{{ route('login') }}">Login Admin</a>
-            </div>
+   <nav class="bg-blue-600 text-white py-4">
+    <div class="container max-w-6xl mx-auto px-4 flex justify-between items-center">
+        <a href="{{ route('homepage')}}" class="text-2xl font-bold">RI-Library</a>
+        <div>
+            <a href="{{ route('homepage') }}" class="px-4">Homepage</a>
+            <a href="{{ route('login') }}" class="px-4">Login</a>
         </div>
-    </nav>
+    </div>
+</nav>
+
 
     @yield('content')
 
